ef5999c5414d52296ac76bb674c2212304296a7e
ryflect <prateek.mallya@hotmail.com>
2020-10-21 23:17:46 -0400

    add get_histplot function

--- reference.py
+++ reference.py
@@ -1,6 +1,13 @@
 # Contains all of the reference functions used in different notebooks 
 # (prototyping of these functions present in Reference.ipynb)
 
+# imports
+import pandas as pd 
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np 
+import seaborn as sns 
+
 # Reference function 1
 # get_file_names(col_names, df_ref)
 #
@@ -19,3 +26,26 @@ def get_file_names(col_names, df_ref):
     # get the intersection of file_names for all cols
     file_names = list(set.intersection(*map(set, file_names)))
     return file_names
+
+
+# Reference function 2
+# get_histplot(col_names, df_ref, labels)
+#
+# Purpose: Returns a histogram of value_counts of column specified in col_name present in df_ref using IDEHR as the counter
+# 
+# Input: col_name: Column name
+#        df_ref: Data
+#        label: Column label
+#
+# Returns: Histplot of the value_counts of the columns
+
+def get_histplot(col_name, df_ref, label):
+    plt.rcParams['figure.figsize'] = [15, 8]
+    matplotlib.rcParams.update({'font.size': 15})
+    plt.figure()
+    fig, ax = plt.subplots()
+    ax = sns.histplot(data=df_ref[['IDEHR', col_name]].dropna().IDEHR.value_counts())
+    ax.set_title(label)
+    ax.set_xlabel('No. of observations')
+    ax.set_ylabel('No. of IDEHRs')
+    plt.show()
